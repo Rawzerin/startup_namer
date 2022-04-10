3eaa96390b2bf64e214e8a8d592fd81098dda4b7
rawzerin fariha <rawzerin@gmail.com>
2022-04-10 14:37:33 +0600

    2nd

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,11 @@ class MyApp extends StatelessWidget {
               label: 'book',
             ),
 
+            BottomNavigationBarItem(
+              icon: Icon(Icons.people),
+              label: 'people',
+            ),
+
           ], //<BottomNavigationItem>[]
 
 
@@ -78,4 +83,138 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
+}
+
+/////////////////////////////////////////////class2///////////////////////////////////////////////////
+class ScrollingParallax extends StatelessWidget {
+  const ScrollingParallax({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // a list hold the Parallax items
+    return SingleChildScrollView(
+      child: Column(
+        children: const [],
+      ),
+    );
+  }
+}
+
+//////////////////////////////////////////class3///////////////
+class LocationListItem extends StatelessWidget {
+  const LocationListItem({
+    Key? key,
+    required this.imageUrl,
+    required this.name,
+    required this.country,
+  }) : super(key: key);
+
+  final String imageUrl;
+  final String name;
+  final String country;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+      child: AspectRatio(
+        aspectRatio: 16 / 9,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(16),
+          child: Stack(
+            children: [
+              _buildParallaxBackground(context),
+              _buildGradient(),
+              _buildTitleAndSubtitle(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildParallaxBackground(BuildContext context) {
+    return Image.network(
+      imageUrl,
+      fit: BoxFit.cover,
+    );
+  }
+
+  Widget _buildGradient() {
+    return Positioned.fill(
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.transparent, Colors.black.withOpacity(0.7)],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            stops: const [0.6, 0.95],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTitleAndSubtitle() {
+    return Positioned(
+      left: 20,
+      bottom: 20,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            name,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            country,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 14,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+////////////////////////////////class4/////////////////////
+//to add parallax effect to the background image//
+class ParallaxFlowDelegate extends FlowDelegate {
+  ParallaxFlowDelegate({
+    required this.scrollable,
+    required this.listItemContext,
+    required this.backgroundImageKey,
+  }) : super(repaint: scrollable.position);
+
+  @override
+  void paintChildren(FlowPaintingContext context) {
+    // Calculate the position of this list item within the viewport.
+    final scrollableBox = scrollable.context.findRenderObject() as RenderBox;
+    final listItemBox = listItemContext.findRenderObject() as RenderBox;
+    final listItemOffset = listItemBox.localToGlobal(
+        listItemBox.size.centerLeft(Offset.zero),
+        ancestor: scrollableBox);
+    // Determine the percent position of this list item within the
+    // scrollable area.
+    final viewportDimension = scrollable.position.viewportDimension;
+    final scrollFraction =
+    (listItemOffset.dy / viewportDimension).clamp(0.0, 1.0);
+  }
+
+  // Calculate the vertical alignment of the background
+  // based on the scroll percent.
+  final verticalAlignment = Alignment(0.0, scrollFraction * 2 - 1);
+}
+
+@override
+bool shouldRepaint(covariant FlowDelegate oldDelegate) {
+  // TODO: implement shouldRepaint
+  throw UnimplementedError();
 }
